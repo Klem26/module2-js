c23040dc8aaff5c5d848c2f72df3c01358d5922f
Klem26 <Kankina26@gmail.com>
2021-01-05 17:52:51 +0200

    Create task-9.js

---
+++ js/task-9.js
@@ -0,0 +1,11 @@
+function getExtremeElements(array) {
+  
+  const firstElement = array[0];
+  
+  const lastElement = array[array.length-1];
+  
+  const newArray = [firstElement ,lastElement];
+  
+  return newArray
+
+  }
